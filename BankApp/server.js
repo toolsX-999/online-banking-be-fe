f7180c630ab34c7a7aad287da17eb916046a0a34
tools x admin <toolsx999@gmail.com>
2025-06-07 03:40:52 -0400

    Fix: Fixed views second time

--- BankApp/server.js
+++ BankApp/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", sitenavRouter);
