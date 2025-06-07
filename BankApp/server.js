d7153a641d3b8b4b75c482df9c6e98a6c467e944
tools x admin <toolsx999@gmail.com>
2025-06-07 03:33:35 -0400

    Fix: Fixed views

--- BankApp/server.js
+++ BankApp/server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/pages'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", sitenavRouter);
